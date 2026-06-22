20343ab1141d6cdf4738b8cdcd1294cc67195a23
mahfuzeee <45316693+mahfuzeee@users.noreply.github.com>
2026-06-22 20:25:52 +0600

    tanstack query ok.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import { useState, useEffect } from "react";
-
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  const [blogs, setBlogs] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("http://localhost:3000/api/v1/blogs");
-      const data = await response.json();
-      setBlogs(data.data);
-      console.log(data);
-    })();
-  }, []);
+  const { isPending, error, data } = useQuery({
+    queryKey: ["blogs"],
+    queryFn: () =>
+      fetch("http://localhost:3000/api/v1/blogs").then((res) => res.json()),
+  });
+  console.log(data);
+  if (isPending) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      {blogs.map((blog) => (
-        <p key={blog._id}>{blog.title}</p>
+      {data["data"].map((blog) => (
+        <div key={blog._id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.content}</p>
+        </div>
       ))}
     </div>
   );
