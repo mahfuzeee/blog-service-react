636b5bd2ea5ddb2ab1e15c8f0a3a26bcb159fd6a
mahfuzeee <45316693+mahfuzeee@users.noreply.github.com>
2026-06-27 23:23:23 +0600

    Add Admin page with link to create post and display all blogs

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import { GetAllBlogs } from "../components/GetAllBlogs";
 function Admin() {
   return (
     <div>
       <h1>Admin Page</h1>
+      <Link to="/create">Create Post</Link>
+      <div>
+        <GetAllBlogs />
+      </div>
     </div>
   );
 }
