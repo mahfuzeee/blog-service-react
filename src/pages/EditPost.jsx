80bb5b2770900a7e7b55c5000630944b26175ac4
mahfuzeee <45316693+mahfuzeee@users.noreply.github.com>
2026-07-05 22:53:08 +0600

    tailwind daisy ui setup ok.

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,7 +1,10 @@
+import CreatePost from "../components/CreatePost";
 function EditPost() {
   return (
     <div>
       <h1>Edit Post</h1>
+      <button className="btn btn-primary">Edit</button>
+      <CreatePost />
     </div>
   );
 }
